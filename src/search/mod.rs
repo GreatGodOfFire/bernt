9a79911a0eac9c1751d4ed22e0c8722b27c00b67
GreatGodOfFire <greatgodoffire@protonmail.com>
2023-10-09 20:24:01 +0200

    Check if plies > 0 in tt cutoffs

--- src/search/mod.rs
+++ src/search/mod.rs
@@ -239,6 +239,7 @@ impl SearchContext<'_> {
             && (tt_ty == TTEntryType::Exact
                 || (tt_ty == TTEntryType::Lower && tt_eval >= beta)
                 || (tt_ty == TTEntryType::Upper && alpha >= tt_eval))
+            && plies > 0
         {
             // TODO: check with is_pseudolegal
             return Some((tt_move, tt_eval));
