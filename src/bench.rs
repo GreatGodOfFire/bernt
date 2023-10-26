46ff7e7d167550d18db77c69abf290c8de907a49
GreatGodOfFire <greatgodoffire@protonmail.com>
2023-10-26 23:35:38 +0200

    Increase bench depth
    Bench: 57424786

--- src/bench.rs
+++ src/bench.rs
@@ -62,7 +62,7 @@ pub const BENCH_FENS: [&str; 50] = [
 pub fn bench() {
     let mut tt = TT::new_default();
     let mut options = SearchOptions::default();
-    options.depth = 4;
+    options.depth = 6;
     options.info = false;
 
     let mut nodes = 0;
