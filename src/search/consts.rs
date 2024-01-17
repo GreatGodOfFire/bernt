afd9d10a448660db54526c94199e973cb4ef47bd
GreatGodOfFire <greatgodoffire@protonmail.com>
2024-01-17 08:31:33 +0100

    SPSA
    Bench: 3737397

--- src/search/consts.rs
+++ src/search/consts.rs
@@ -1,32 +1,32 @@
-pub const ASP_DEPTH: u8 = 4;
-pub const ASP_WINDOW: i32 = 114;
-pub const ASP_INC_FACTOR: f32 = 0.47;
+pub const ASP_DEPTH: u8 = 3;
+pub const ASP_WINDOW: i32 = 101;
+pub const ASP_INC_FACTOR: f32 = 0.75;
 
 pub const NMP_REDUCTION: u8 = 3;
 
-pub const LMR_BASE: f32 = 1.13;
-pub const LMR_DIV: f32 = 3.25;
+pub const LMR_BASE: f32 = 1.43;
+pub const LMR_DIV: f32 = 1.25;
 pub const LMR_NMOVES: u16 = 5;
 
-pub const LMP_DEPTH: u8 = 2;
-pub const LMP_BASE: u16 = 2;
+pub const LMP_DEPTH: u8 = 3;
+pub const LMP_BASE: u16 = 3;
 pub const LMP_MUL: u16 = 5;
-pub const LMP_POW: u32 = 2;
+pub const LMP_POW: u32 = 1;
 
-pub const RFP_DEPTH: u8 = 3;
-pub const RFP_MARGIN: i32 = 82;
+pub const RFP_DEPTH: u8 = 4;
+pub const RFP_MARGIN: i32 = 74;
 
-pub const FP_DEPTH: u8 = 5;
-pub const FP_BASE: i32 = 259;
-pub const FP_MUL: i32 = 527;
+pub const FP_DEPTH: u8 = 4;
+pub const FP_BASE: i32 = 231;
+pub const FP_MUL: i32 = 504;
 
-pub const TIMEMAN_HARDDIV: f32 = 3.79;
-pub const TIMEMAN_SOFTDIV: f32 = 44.61;
-
-pub const HIST_MUL: i32 = 365;
-pub const HIST_ADD: i32 = -427;
-pub const CONTHIST_MUL: i32 = 365;
+pub const HIST_MUL: i32 = 287;
+pub const HIST_ADD: i32 = -428;
+pub const CONTHIST_MUL: i32 = 414;
 pub const CONTHIST_ADD: i32 = -427;
+
+pub const TIMEMAN_HARDDIV: f32 = 3.31;
+pub const TIMEMAN_SOFTDIV: f32 = 53.46;
 
 #[rustfmt::skip]
 pub const MVVLVA_LOOKUP: [[i32; 5]; 6] = [
